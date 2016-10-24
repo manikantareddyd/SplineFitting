eab0766ad223e780f7e7c7ada848e19ed5406122
Manikanta Reddy D <manikanta.reddy.d@live.com>
2016-10-24 13:55:33 +0530

    Done with Quad Spline... Improvements in Plotting

--- QuadraticSpline.py
+++ QuadraticSpline.py
@@ -13,23 +13,33 @@ with open("input.txt") as f:
         so = float(line.split(" ")[0])
         sn = float(line.split(" ")[1])
 
-print "Linear Spline"
-def get_prediction(x,y,xstar):
+import numpy as np
+n = len(x)
+A = np.tri(n,n) - np.tri(n,n,-2)
+A = A.astype(float)
+d = [0.0 for i in range(n)]
+for i in range(1,n):
+    d[i] = 2*(y[i]-y[i-1])/(x[i]-x[i-1])
+
+z = np.linalg.solve(A,d)
+
+def get_prediction(x,y,z,xstar):
     try:
         i = next(v[0] for v in enumerate(x) if v[1] > xstar)
-        return ((xstar - x[i-1])*y[i]/(x[i]-x[i-1])) + ((xstar - x[i])*y[i-1]/(x[i-1]-x[i]))
+        return (z[i-1]*(xstar - x[i-1])) + ((z[i]-z[i-1])*((xstar - x[i-1])**2)/(2*(x[i]-x[i-1]))) + y[i-1]
     except:
         return y[-1]
-    
+
 yt = []
 for xstar in xt:
-    ystar = get_prediction(x,y,xstar)
+    ystar = get_prediction(x,y,z,xstar)
     yt.append(ystar)
     print xstar,"\t",ystar
+
 import random
 t = [random.random()*(max(x) - min(x)) + min(x) for i in range(10*len(x))] + x
 t.sort()
-ty = [get_prediction(x,y,t[i]) for i in range(len(t))]
+ty = [get_prediction(x,y,z,t[i]) for i in range(len(t))]
 
 
 import matplotlib.pyplot as plt
